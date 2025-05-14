563890239a7a9a99223d1dc176e7389ec2979a0c
Zuppah1 <mantvydas.rinkunas@gmail.com>
2025-05-14 18:00:45 +0300

    maze class, unit tests

---
+++ maze.py
@@ -0,0 +1,48 @@
+import time
+from cell import Cell
+from graphics import Window
+
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = []
+        for i in range(0, self._num_cols):
+            column = []
+            for j in range(0, self._num_rows):
+                cell = Cell(self._win)
+                column.append(cell)
+            self._cells.append(column)
+        for i in range(0, self._num_cols):
+            for j in range(0, self._num_rows):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        x1 = self._x1
+        y1 = self._y1
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+
+        self._cells[i][j].draw(
+            (x1 + (self._cell_size_x * i)), (y1 + (self._cell_size_y * j)),
+            (x2 + (self._cell_size_x * i)), (y2 + (self._cell_size_y * j))
+        )
+
+        self._animate()
+
+    def _animate(self):
+        if self._win:
+            self._win.redraw()
+            time.sleep(0.02)
+
+
+
